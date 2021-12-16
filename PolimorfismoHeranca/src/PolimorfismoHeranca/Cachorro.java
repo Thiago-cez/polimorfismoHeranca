8048652ce42e344fd269eb96938eedc14c064cc9
Thiago-Cez <thiago.silva.ts804@gmail.com>
2021-12-15 23:19:45 -0300

    Polimorfismo e Herança

---
+++ PolimorfismoHeranca/src/PolimorfismoHeranca/Cachorro.java
@@ -0,0 +1,10 @@
+package PolimorfismoHeranca;
+
+public class Cachorro extends Animal{
+	void emitir() {
+		System.out.println("Latindo ");
+	}
+	void correr() {
+		System.out.println("O cachorro está correndo Latindo!");
+	}
+}
---
+++ PolimorfismoHeranca/src/PolimorfismoHeranca/Preguica.java
@@ -0,0 +1,10 @@
+package PolimorfismoHeranca;
+
+public class Preguica extends Animal{
+	void emitir() {
+		System.out.println("Zzzzzzzzzz!");
+	}
+	void subir() {
+		System.out.println("A preguiça está subindo no tronco");
+	}
+}
